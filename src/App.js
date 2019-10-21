22129ed9475886fb7d51d7878135b88faad507f7
lindyloutama <ltamakaha@yahoo.com>
2019-10-21 12:12:33 +0100

    adding changes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,41 @@ import Item from "./Item";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+      tasks: [
+        {text: "walk the cat", completed: true, date: "2019-10-16"},
+        {text: "buy the oats", completed: false, date: "2019-10-18"},
+        {text: "learn react", completed: false, date: "2019-10-19"},
+        {text: "wash slippers", completed: true, date: "2019-10-25"},
+        {text: "de-flea the cat", completed: false, date: "2019-11-10"},
+        {text: "create insta account", completed: true, date: "2019-10-30"}
+      ]
+    }
+
+// Function to update the tasks with a new task
+  addTask = (taskText) => {
+    console.log(taskText);
+    // Create a new task (with default completed and date properties)
+
+    // Add that task to the state
+  }
+
   render() {
     return (
       <div className="container">
-        <h1 id="mainHeading">Todo List</h1>
+        <h1 id="mainHeading">Todo List <i className="fas fa-camera"></i></h1>
         <h3>Things I need to do very soon.</h3>
         <div className="row">
           <div className="col-12 col-lg-6">
             <AddItem />
           </div>
           <div className="col-12 col-lg-6">
-            <ItemCount count={5} />
+            <ItemCount count={this.state.tasks.length} />
             <ul id="itemList">
-              <Item text="walk the cat" completed={false} />
-              <Item text="buy oats" completed={true} />
-              <Item text="learn React" completed={true}/>
-              <Item text="wash slippers" completed={true}/>
-              <Item text="de-flea the cat" completed={false} />
+              {this.state.tasks.map(item =>{
+                return <Item text={item.text} completed={item.completed} date={item.date}/>
+              })}
+
             </ul>
           </div>
         </div>
--- src/Item.js
+++ src/Item.js
@@ -12,11 +12,13 @@ class Item extends React.Component {
             </span>
           </div>
           <div className="col-2">
-            <button className="btn btn-info">Delete</button>
+            {/* <button className="btn btn-info">Delete</button> */}
+            <i class="far fa-trash-alt redBins"></i>
           </div>
           <div className="col-2">
             {/* {this.props.completed ? <button className="btn btn-light">uncheck</button> : <button className="btn btn-light">Done</button>} */}
-            {this.props.completed === false && <button className="btn btn-light">Done</button>}
+            {/* {this.props.completed === false && <button className="btn btn-light">Done</button>} */}
+            <i class="fas fa-check greenCheck"></i>
             {/* {this.props.completed === true && <button className="btn btn-light">uncheck</button>} */}
           </div>
         </div>
